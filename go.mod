2852aa0c733fae13ad9826052eaea60a470616fa
Andrew Rodionoff <andviro@gmail.com>
2023-10-30 02:06:27 +0300

    [tx] purged multierror

--- go.mod
+++ go.mod
@@ -6,14 +6,12 @@ require (
 	github.com/andviro/goldie v0.0.0-20180822203610-4d8717fa0de8
 	github.com/go-gormigrate/gormigrate/v2 v2.1.0
 	github.com/go-mixins/log v0.2.6
-	github.com/hashicorp/go-multierror v1.1.1
 	github.com/oleiade/reflections v1.0.1
 	gorm.io/driver/sqlite v1.5.2
 	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55
 )
 
 require (
-	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
--- tx.go
+++ tx.go
@@ -1,9 +1,8 @@
 package gorm
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/hashicorp/go-multierror"
 )
 
 func (b *Backend) Begin() *Backend {
@@ -15,16 +14,15 @@ func (b *Backend) Begin() *Backend {
 
 func (b *Backend) End(rErr error) error {
 	if e := recover(); e != nil {
-		rErr = multierror.Append(rErr, fmt.Errorf("panic: %+v", e))
+		rErr = errors.Join(rErr, fmt.Errorf("panic: %+v", e))
 		defer panic(rErr)
 	}
 	if rErr != nil {
 		if err := b.DB.Rollback().Error; err != nil {
-			return multierror.Append(rErr, fmt.Errorf("rolling back: %w", err))
+			return errors.Join(rErr, fmt.Errorf("rolling back: %+v", err))
 		}
 		return rErr
-	}
-	if err := b.DB.Commit().Error; err != nil {
+	} else if err := b.DB.Commit().Error; err != nil {
 		return fmt.Errorf("committing changes: %w", err)
 	}
 	return nil
